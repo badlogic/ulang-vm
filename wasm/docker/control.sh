56c2f655b2744576bfd3163bc49dc1ce64c7e255
Mario Zechner <badlogicgames@gmail.com>
2022-03-20 03:10:25 +0100

    No more need for no-cache builds.

--- wasm/docker/control.sh
+++ wasm/docker/control.sh
@@ -34,7 +34,7 @@ restart)
 	;;
 start)
 	git pull
-	docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml build --no-cache
+	docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml build
 	docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml up -d
 	;;
 startdev)	
