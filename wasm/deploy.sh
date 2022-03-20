612e62522d44142c8b99877ca3f82c76acf36974
Mario Zechner <badlogicgames@gmail.com>
2022-03-20 03:33:32 +0100

    No more sudo for deployment.

--- wasm/deploy.sh
+++ wasm/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-ssh -t ulang.io "cd ulang.io/wasm &&  sudo ./docker/control.sh stop && sudo git pull && sudo ./docker/control.sh start && sudo ./docker/control.sh logs"
+ssh -t ulang.io "cd ulang.io/wasm && ./docker/control.sh stop && git pull && ./docker/control.sh start && ./docker/control.sh logs"
