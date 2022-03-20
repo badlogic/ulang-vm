18fb6db55c957d744b433e9500bb38a74f96fa59
Mario Zechner <badlogicgames@gmail.com>
2022-03-20 03:19:10 +0100

    Don't try live-edit if the endpoint doesn't exist.

--- wasm/client/src/liveedit.ts
+++ wasm/client/src/liveedit.ts
@@ -1,14 +1,20 @@
 import { Socket, io } from "socket.io-client"
 
 export async function setupLiveEdit () {
-	let lastChangeTimestamp = null;
-	this.socket = io({ path: "/ws", transports: ['websocket'], reconnectionAttempts: 0 });
-	this.socket.on("connect", () => console.log("Connected"));
-	this.socket.on("disconnect", () => console.log("Disconnected"));
-	this.socket.on("message", (timestamp) => {
-		if (lastChangeTimestamp != timestamp) {
-			setTimeout(() => location.reload(), 100);
-			lastChangeTimestamp = timestamp;
+	fetch("/ws").then((res) => {
+		if (!res.ok) {
+			console.log("No live-edit available.");
+			return;
 		}
+		let lastChangeTimestamp = null;
+		this.socket = io({ path: "/ws", transports: ['websocket'] });
+		this.socket.on("connect", () => console.log("Connected"));
+		this.socket.on("disconnect", () => console.log("Disconnected"));
+		this.socket.on("message", (timestamp) => {
+			if (lastChangeTimestamp != timestamp) {
+				setTimeout(() => location.reload(), 100);
+				lastChangeTimestamp = timestamp;
+			}
+		});
 	});
 };
