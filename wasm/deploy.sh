0054d1477cb38949fa00c7ccba06c4e64b94cacc
Mario Zechner <badlogicgames@gmail.com>
2022-03-20 21:08:33 +0100

    Fix deploy script.

--- wasm/deploy.sh
+++ wasm/deploy.sh
@@ -4,4 +4,6 @@ if [ -z "ULANG_CLIENT_SECRET" ]; then
 	echo "Set ULANG_CLIENT_SECRET from the GitHub OAuth app settings."
 	exit 1
 fi
-ssh -t ulang.io "cd ulang.io/wasm && ./docker/control.sh stop && ULANG_CLIENT_SECRET=${ULANG_CLIENT_SECRET} git pull && ./docker/control.sh start && ./docker/control.sh logs"
+CMD="cd ulang.io/wasm && ./docker/control.sh stop && git pull && ULANG_CLIENT_SECRET=${ULANG_CLIENT_SECRET} ./docker/control.sh start && ./docker/control.sh logs"
+echo $CMD
+ssh -t ulang.io $CMD
