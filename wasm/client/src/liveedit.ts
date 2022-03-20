dbb2cf63268ff5295c4af4b8b4af54676a3ff6c5
Mario Zechner <badlogicgames@gmail.com>
2022-03-20 03:16:44 +0100

    New deployment script, no live-edit reconnects.

--- wasm/client/src/liveedit.ts
+++ wasm/client/src/liveedit.ts
@@ -2,7 +2,7 @@ import { Socket, io } from "socket.io-client"
 
 export async function setupLiveEdit () {
 	let lastChangeTimestamp = null;
-	this.socket = io({ path: "/ws", transports: ['websocket'] });
+	this.socket = io({ path: "/ws", transports: ['websocket'], reconnectionAttempts: 0 });
 	this.socket.on("connect", () => console.log("Connected"));
 	this.socket.on("disconnect", () => console.log("Disconnected"));
 	this.socket.on("message", (timestamp) => {
--- wasm/deploy.sh
+++ wasm/deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 set -e
-npm run clean
-npm run build:wasm
-npm run build:vm
-npm run build:client
-npm run build:server
-npm run minify
-
-rsync -av ulang-site/assets/ marioslab.io:/home/badlogic/marioslab.io/data/web/ulang
+ssh -t ulang.io "cd ulang.io/wasm &&  sudo ./docker/control.sh stop && sudo git pull && sudo ./docker/control.sh start && sudo ./docker/control.sh logs"
