9778d34c89c079affa9aa9d8f81b4f634927185f
Mario Zechner <badlogicgames@gmail.com>
2022-03-21 16:41:31 +0100

    Fix docker deployment.

--- wasm/deploy.sh
+++ wasm/deploy.sh
@@ -8,6 +8,6 @@ if [ -z "ULANG_CLIENT_ID" ]; then
 	echo "Set ULANG_CLIENT_ID from the GitHub OAuth app settings."
 	exit 1
 fi
-CMD="cd ulang.io/wasm && ./docker/control.sh stop && git pull && ULANG_CLIENT_SECRET=${ULANG_CLIENT_SECRET} ULANG_CLIENT_ID=${ULANG_CLIENT_ID} ./docker/control.sh start && ./docker/control.sh logs"
+CMD="export ULANG_CLIENT_SECRET=${ULANG_CLIENT_SECRET} && export  ULANG_CLIENT_ID=${ULANG_CLIENT_ID} && cd ulang.io/wasm && ./docker/control.sh stop && git pull && ./docker/control.sh start && ./docker/control.sh logs"
 echo $CMD
 ssh -t ulang.io $CMD
