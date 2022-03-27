fe5ea6d39b794fa2eb491fba0ff8c2edf2409a02
Mario Zechner <badlogicgames@gmail.com>
2022-03-27 23:10:32 +0200

    Timeout docker stop after 1 second.

--- wasm/docker/control.sh
+++ wasm/docker/control.sh
@@ -46,7 +46,7 @@ reloadnginx)
 	docker exec -it ulang_nginx nginx -s reload
 	;;
 stop)
-	docker-compose -f docker-compose.base.yml down
+	docker-compose -f docker-compose.base.yml down -t 1
 	;;
 shell)
 	docker exec -it ulang_site bash
